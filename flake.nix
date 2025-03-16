10990d49ceeade1a5a5281b9af6f2fcbcd423a21
orhnk <utfeightt@gmail.com>
2025-03-16 18:20:54 +0300

    package for nix

--- flake.nix
+++ flake.nix
@@ -26,19 +26,50 @@
         nativeBuildInputs = [
           pkgs.qt5.wrapQtAppsHook
           pkgs.makeWrapper
+          pkgs.copyDesktopItems
         ];
+
         buildInputs = [pythonEnv];
+
+        desktopItems = [
+          (pkgs.makeDesktopItem {
+            name = "kohighlights";
+            desktopName = "KoHighlights";
+            exec = "kohighlights";
+            icon = "kohighlights";
+            categories = ["Utility"];
+            comment = "Kindle Highlights Manager";
+            terminal = false;
+          })
+        ];
 
         installPhase = ''
           mkdir -p $out/bin $out/share/kohighlights
-          # Copy all Python files and local modules
+
+          # Copy application files
           cp *.py $out/share/kohighlights/
 
+          # Copy icons
+          mkdir -p $out/share/icons/hicolor/48x48/apps
+          cp assets/icon-48x48.png $out/share/icons/hicolor/48x48/apps/kohighlights.png
+
+          # Create wrapper script
           makeWrapper ${pythonEnv}/bin/python $out/bin/kohighlights \
             --add-flags "$out/share/kohighlights/main.py" \
             --set QT_DEBUG_PLUGINS "1" \
             --prefix PYTHONPATH : "$out/share/kohighlights" \
             --prefix QT_PLUGIN_PATH : "${pkgs.qt5.qtbase.bin}/${pkgs.qt5.qtbase.qtPluginPrefix}"
+
+          # Install desktop file (handled automatically by copyDesktopItems)
+          install -Dm644 ${pkgs.makeDesktopItem {
+            name = "kohighlights";
+            desktopName = "KoHighlights";
+            exec = "$out/bin/kohighlights";
+            icon = "kohighlights";
+            categories = ["Utility"];
+            comment = "Kindle Highlights Manager";
+            terminal = false;
+          }}/share/applications/* $out/share/applications/kohighlights.desktop
         '';
       };
 
